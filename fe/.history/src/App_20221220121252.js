8edfa4a830093f34c5e1e636f9414cf9d4e177c3
Hoang Duy <19521425@gm.uit.edu.vn>
2022-12-20 12:21:11 +0700

    first commit

---
+++ fe/.history/src/App_20221220121252.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return <h1>hello</h1>;
+}
+
+export default App;
